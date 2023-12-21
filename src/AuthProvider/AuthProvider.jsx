e7f08de1252aa102b550627d870815b9f69fe08a
Md-ShakilHossain <shakilh550@gmail.com>
2023-12-22 01:41:01 +0600

    made auth provider

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,73 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import auth from "../Firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        });
+    }
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe;
+        }
+    }, [])
+
+    const data = {
+        user,
+        createUser,
+        loginUser,
+        logOut,
+        loading,
+        googleSignIn,
+        updateUserProfile
+    }
+
+    return (
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
+
+export default AuthProvider;
