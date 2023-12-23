94ad6bdd2a72c8ad3fb61e7556e8a4940cfcf587
Md-ShakilHossain <shakilh550@gmail.com>
2023-12-23 13:26:01 +0600

    made delete button functional

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import useAuth from "../Hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../Hooks/useAxiosPublic";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Dashboard = () => {
 
@@ -17,6 +18,33 @@ const Dashboard = () => {
         }
     });
     refetch();
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosPublic.delete(`/tasks/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    };
+
 
     return (
         <div className="mt-12">
@@ -45,7 +73,7 @@ const Dashboard = () => {
 
                                 <div className="flex gap-4 justify-center mt-2">
                                     <Link to={`/tasks/${task._id}`}><button><FaEdit></FaEdit></button></Link>
-                                    <button><FaTrash></FaTrash></button>
+                                    <button onClick={() => handleDelete(task._id)}><FaTrash></FaTrash></button>
                                 </div>
                             </div>)
                         }
